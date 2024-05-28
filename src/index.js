607ba8776b711c07c32086c7a7a9de749fe16bf2
Alex <alex.kleinman@gmail.com>
2024-05-28 09:36:30 -0400

    added click event listeners to each filter slot

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+const blingDisplay = document.querySelector("#BlingDisplay");
+
 const blingRender = () => {
-  const blingDisplay = document.querySelector("#BlingDisplay");
-  
   fetch(`http://localhost:3000/data`)
   .then((response) => response.json())
   .then((data) => {
@@ -13,8 +13,52 @@ const blingRender = () => {
     });
   })
   }
-  
-  const main = document.addEventListener("DOMContentLoaded", () => {
+
+const armSlotClickHandler = () => {
+  const armSlot = document.querySelector("#ArmDisplay > img");
+
+  armSlot.addEventListener("click", () => {
+    console.log("click");
+    blingDisplay.filter((bling) => bling.category = "Gauntlets")
+})
+}
+
+const headSlotClickHandler = () => {
+  const headSlot = document.querySelector("#HelmDisplay > img")
+
+  headSlot.addEventListener("click", () => {
+    console.log("click");
+    blingDisplay.filter((bling) => bling.category = "Helm")
+  })
+}
+
+const torsoSlotClickHandler = () => {
+  const torsoSlot = document.querySelector("#BreastPlateDisplay > img");
+
+  torsoSlot.addEventListener("click", () => {
+    console.log("click");
+    blingDisplay.filter((bling) => bling.category = "Chest Armor")
+  })
+}
+
+const legSlotClickHandler = () => {
+  const legSlot = document.querySelector("#LegDisplay > img");
+
+  legSlot.addEventListener("click", () => {
+    console.log("click");
+    blingDisplay.filter((bling) => bling.category = "Leg Armor")
+})
+}
+
+const filterClickHandler = () => {
+  torsoSlotClickHandler();
+  armSlotClickHandler();
+  legSlotClickHandler();
+  headSlotClickHandler();
+}
+
+const main = document.addEventListener("DOMContentLoaded", () => {
     blingRender();
+    filterClickHandler();
   })
   
