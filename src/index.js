e57e330399fde3c61a7109a91b992a19ca1e032f
Alex <alex.kleinman@gmail.com>
2024-05-29 13:53:44 -0400

    turned filter events into a renderer + callback function

--- src/index.js
+++ src/index.js
@@ -5,10 +5,22 @@ const blingRender = () => {
   fetch(`http://localhost:3000/data`)
   .then((response) => response.json())
   .then((data) => {
-      blingData.push(...data)
+      blingData.push(...data);
+      renderFilter(data);
       renderBling(data);
     });
   }
+
+const renderFilter = (data) => {
+  const armSlot = document.querySelector("#ArmDisplay > img");
+  const headSlot = document.querySelector("#HelmDisplay > img");
+  const torsoSlot = document.querySelector("#BreastPlateDisplay > img");
+  const legSlot = document.querySelector("#LegDisplay > img");
+  handleFilterClick(data, "Gauntlets", armSlot);
+  handleFilterClick(data, "Helm", headSlot);
+  handleFilterClick(data, "Chest Armor", torsoSlot);
+  handleFilterClick(data, "Leg Armor", legSlot);
+}
 
 const renderBling = (data) => {
     blingDisplay.innerHTML = "";
@@ -29,73 +41,56 @@ const renderBling = (data) => {
       divElement.appendChild(textDiv)
       textDiv.appendChild(headerElement);
       textDiv.appendChild(paraElement);
-      divElement.addEventListener("mouseover", () => {
-        handleMouseOver(imgElement, textDiv);
+      handleMouseOver(divElement, imgElement, textDiv);
+      handleMouseOut(divElement, imgElement, textDiv);
        })
-      divElement.addEventListener("mouseout", () => {
-        handleMouseOut(imgElement, textDiv);
-      })
-      })
+      }
+      
   
-  const handleMouseOver = (imgElement, textDiv) => {
+  const handleMouseOver = (divElement, imgElement, textDiv) => {
+      divElement.addEventListener("mouseover", () => {
       imgElement.style.display = "none";
       textDiv.style.display = "block";
-      }
+      })
     }
 
-  const handleMouseOut = (imgElement, textDiv) => {
+  const handleMouseOut = (divElement, imgElement, textDiv) => {
+      divElement.addEventListener("mouseout", () => {
       imgElement.style.display = "block";
       textDiv.style.display = "none";
-    }
-
-  const armSlotClickHandler = () => {
-    const armSlot = document.querySelector("#ArmDisplay > img");
-  
-    armSlot.addEventListener("click", () => {
+    })
+  }
+    
+  const handleFilterClick = (data, category, element) => {
+      
+    element.addEventListener("click", () => {
       console.log("click");
-      const filteredBling = blingData.filter((bling) => bling.category === "Gauntlets");
+      const filteredBling = data.filter((data) => data.category === category);
       renderBling(filteredBling);
   })
+}
+
+/* const filterClickHandler2 = (input, filter, blingData) => {
+  const helmFilter = blingData.filter((bling) => bling.category === "Helm") 
+  const gauntletFilter = blingData.filter((bling) => bling.category === "Gauntlets")
+  const chestFilter = blingData.filter((bling) => bling.category === "Chest Armor")
+  const legFilter = blingData.filter((bling) => bling.category === "Leg Armor")
+
+  input.addEventListener("click", () => { 
+  console.log("click")
+  const filteredBling = blingData.filter((bling) => bling.category === filter)
+
   }
 
-const headSlotClickHandler = () => {
-  const headSlot = document.querySelector("#HelmDisplay > img")
 
-  headSlot.addEventListener("click", () => {
-    console.log("click");
-    const filteredBling = blingData.filter((bling) => bling.category === "Helm");
-    renderBling(filteredBling);
-  })
-}
+   )
+  
+  { 
 
-const torsoSlotClickHandler = () => {
-  const torsoSlot = document.querySelector("#BreastPlateDisplay > img");
+} */
 
-  torsoSlot.addEventListener("click", () => {
-    console.log("click");
-    const filteredBling = blingData.filter((bling) => bling.category === "Chest Armor");
-    renderBling(filteredBling);
-  })
-}
-
-const legSlotClickHandler = () => {
-  const legSlot = document.querySelector("#LegDisplay > img");
-
-  legSlot.addEventListener("click", () => {
-    console.log("click");
-    const filteredBling = blingData.filter((bling) => bling.category === "Leg Armor");
-    renderBling(filteredBling);
-})
-}
-
-const filterClickHandler = () => {
-  torsoSlotClickHandler();
-  armSlotClickHandler();
-  legSlotClickHandler();
-  headSlotClickHandler();
-}
 
 const main = document.addEventListener("DOMContentLoaded", () => {
     blingRender();
-    filterClickHandler();
+    renderFilter();
   })
